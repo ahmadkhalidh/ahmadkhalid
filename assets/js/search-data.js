a59c9915ef2ddc10f2fd6ef94b46b514e695fbb2
ahmadkhalidh <86327416+ahmadkhalidh@users.noreply.github.com>
2025-08-20 20:48:12 +0000

    Deploying to gh-pages from @ ahmadkhalidh/ahmadkhalid@8b21f11062cf9749546fad1d159239c889f512aa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,16 +412,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/ahmadkhalid/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/ahmadkhalid/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-i-graduated-with-first-class-honours-from-federal-university-of-lokoja",
+          title: 'I graduated with First Class Honours from Federal University of Lokoja.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
